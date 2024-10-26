0452976e224e5421fc094164fc27c08fbc9ec245
Ronalds Upenieks <ronaldsupenieks96@gmail.com>
2024-10-26 12:47:27 +0200

    Add DB, add prisma

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AssistantModule } from './modules/assistant/assistant.module';
 import { ResumeParserModule } from './modules/resume-parser/resume-parser.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { PrismaModule } from './modules/prisma/prisma.module';
 
 @Module({
-  imports: [AssistantModule, ResumeParserModule, AuthModule],
+  imports: [AssistantModule, ResumeParserModule, AuthModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
