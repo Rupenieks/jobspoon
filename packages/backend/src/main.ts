d65e4b3db89691c263d2507982997a37e3451add
Ronalds Upenieks <ronaldsupenieks96@gmail.com>
2024-10-30 20:00:01 +0100

    Allow larger payload

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import 'multer';
+import { json } from 'express';
 
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
+  app.use(json({ limit: '50mb' }));
   app.enableCors();
   await app.listen(process.env.PORT || 3000);
 }
