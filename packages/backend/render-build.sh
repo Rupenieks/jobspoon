996f11560c16447f9d0e1ef9aaf9d3cedb66130e
Ronalds Upenieks <32895755+Rupenieks@users.noreply.github.com>
2024-11-29 09:33:18 +0100

    Update render-build.sh

--- packages/backend/render-build.sh
+++ packages/backend/render-build.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
-# exit on errorset -o errexit
+# exit on error
+set -o errexit
 
 # npm install
 # npm run build # uncomment if required
 
 # Store/pull Puppeteer cache with build cache
-if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then 
+if [[ ! -d "${PUPPETEER_CACHE_DIR}" ]]; then 
   echo "...Copying Puppeteer Cache from Build Cache" 
-  cp -R $XDG_CACHE_HOME/puppeteer/ $PUPPETEER_CACHE_DIR
+  mkdir -p "${PUPPETEER_CACHE_DIR}"
+  cp -R "${XDG_CACHE_HOME}/puppeteer/"* "${PUPPETEER_CACHE_DIR}/" || true
 else 
   echo "...Storing Puppeteer Cache in Build Cache" 
-  cp -R $PUPPETEER_CACHE_DIR $XDG_CACHE_HOME
+  mkdir -p "${XDG_CACHE_HOME}/puppeteer"
+  cp -R "${PUPPETEER_CACHE_DIR}"/* "${XDG_CACHE_HOME}/puppeteer/" || true
 fi
