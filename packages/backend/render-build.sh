63d8ef0c9aabeffccd2694642f0bf4404f02eee5
Ronalds Upenieks <32895755+Rupenieks@users.noreply.github.com>
2024-11-29 09:50:34 +0100

    Update render-build.sh

--- packages/backend/render-build.sh
+++ packages/backend/render-build.sh
@@ -15,12 +15,10 @@ echo "XDG_CACHE_HOME: ${XDG_CACHE_HOME}"
 : "${XDG_CACHE_HOME:?XDG_CACHE_HOME is not set}"
 
 # Store/pull Puppeteer cache with build cache
-if [[ ! -d "${PUPPETEER_CACHE_DIR}" ]]; then 
+if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then 
   echo "...Copying Puppeteer Cache from Build Cache" 
-  mkdir -p "${PUPPETEER_CACHE_DIR}"
-  cp -R "${XDG_CACHE_HOME}/puppeteer/"* "${PUPPETEER_CACHE_DIR}/" || true
+  cp -R $XDG_CACHE_HOME/puppeteer/ $PUPPETEER_CACHE_DIR
 else 
   echo "...Storing Puppeteer Cache in Build Cache" 
-  mkdir -p "${XDG_CACHE_HOME}/puppeteer"
-  cp -R "${PUPPETEER_CACHE_DIR}"/* "${XDG_CACHE_HOME}/puppeteer/" || true
+  cp -R $PUPPETEER_CACHE_DIR $XDG_CACHE_HOME
 fi
